ff77e9a2173bb1fddcce1ba5252eb9eb2c0a0765
germanpagano <sergiogermanpagano@gmail.com>
2022-06-24 01:38:37 -0300

    Updates

---
+++ precache-manifest.fedb1ad6bba08b3b7c3968ee5e00104a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f74081fe6631bde0362",
+    "url": "/Form-react-app/static/css/main.550d17c0.chunk.css"
+  },
+  {
+    "revision": "8f74081fe6631bde0362",
+    "url": "/Form-react-app/static/js/main.8f74081f.chunk.js"
+  },
+  {
+    "revision": "c9e4e5feb79923a061e8",
+    "url": "/Form-react-app/static/js/1.c9e4e5fe.chunk.js"
+  },
+  {
+    "revision": "86cf31253bd46c0106cb",
+    "url": "/Form-react-app/static/js/runtime~main.86cf3125.js"
+  },
+  {
+    "revision": "7bd51ca313936e4d79a46bd7932c30fc",
+    "url": "/Form-react-app/static/media/js.7bd51ca3.png"
+  },
+  {
+    "revision": "28e853dcf10047b47d47485ecc8f73dd",
+    "url": "/Form-react-app/index.html"
+  }
+];
